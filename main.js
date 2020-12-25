6a58d557172ca71083b57f800ef666552be01a87
kk3june <kk3june@gmail.com>
2020-12-25 14:39:22 +0900

    complete

--- main.js
+++ main.js
@@ -3,19 +3,31 @@ const color = document.querySelector('.colorInfo');
 const hex = color.querySelector('.hex');
 const chgBtn = document.querySelector('.clickBtn');
 
-function hexchange() {
-    const hexCompo = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b','c','d','e','f'];
-    // console.log(hexCompo[`${Math.floor(Math.random()*17)}`]);
-    let hexInfo = "#" + hexCompo[(Math.floor(Math.random()*17))] + hexCompo[(Math.floor(Math.random()*17))] + hexCompo[(Math.floor(Math.random()*17))] + hexCompo[(Math.floor(Math.random()*17))] + hexCompo[(Math.floor(Math.random()*17))] + hexCompo[(Math.floor(Math.random()*17))];
-    console.log(hexInfo);
-    console.log(hexInfo[0]);
-    console.log(hexInfo[16]);
 
+// function hexGenerator() {
+  //     const hexCompo = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b','c','d','e','f'];
+  //     // console.log(hexCompo[`${Math.floor(Math.random()*16)}`]);
+  //     let hexInfo = "#" + hexCompo[(Math.floor(Math.random()*16))] + hexCompo[(Math.floor(Math.random()*16))] + hexCompo[(Math.floor(Math.random()*16))] + hexCompo[(Math.floor(Math.random()*16))] + hexCompo[(Math.floor(Math.random()*16))] + hexCompo[(Math.floor(Math.random()*16))];
+  
+  //     hex.innerHTML = hexInfo;
+  //     container.style.background = hexInfo;
+  // }
+  
+  function hexGenerator(length) {
+    let hexCompo = '0123456789ABCDEF';
+    let randomHex = '';
+  
+    for ( let i = 0; i < 6; i++) {
+      randomHex += hexCompo.charAt(Math.floor(Math.random()*16));
+    }
+    
+    let hexInfo = '#' + randomHex
     hex.innerHTML = hexInfo;
     container.style.background = hexInfo;
-}
+  }
 
 
-chgBtn.addEventListener('click', () => {
-    hexchange();
-})
+  chgBtn.addEventListener('click', () => {
+    hexGenerator();
+  })
+  
